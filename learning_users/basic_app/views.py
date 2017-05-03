f52d4ca223cd179da94f2f675a074578cd235dad
Sampanna01S <sampanna@us.ibm.com>
2017-05-03 13:55:22 -0700

    Minor fixes

--- learning_users/basic_app/views.py
+++ learning_users/basic_app/views.py
@@ -11,14 +11,12 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
-    print('222 {}'.format(request.method))
     return render(request, 'basic_app/index.html')
 
 def register(request):
     registered = False
 
     if request.method =='POST':
-        print('111 {}'.format(request.POST))
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileInfoForm(data=request.POST)
 
